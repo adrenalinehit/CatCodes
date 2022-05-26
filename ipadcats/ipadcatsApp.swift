8dee0c73aa6f1975a849139755a9504b91670f78
Nick Payne <nick.payne2@skybettingandgaming.com>
2022-05-26 17:18:35 +0100

    Initial Commit

---
+++ ipadcats/ipadcatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ipadcatsApp.swift
+//  ipadcats
+//
+//  Created by Nick Payne on 26/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ipadcatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
