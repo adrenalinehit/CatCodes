06e1b7c2e8cfc14ab765740752a82bee2126a4f2
Nick Payne <nick.payne2@skybettingandgaming.com>
2022-05-30 15:11:22 +0100

    sharing

--- ipadcats/SideView.swift
+++ ipadcats/SideView.swift
@@ -133,16 +133,44 @@ struct IndividualCode: View {
             
         }
         
-        Button {
-            favouriteItem()
-        } label: {
-            appPrefs.isFavourite(animal: animalType, code: statusCode) ? Label("Favourite", systemImage: "star.fill") : Label("Favourite", systemImage: "star")
-        }.buttonStyle(PlainButtonStyle())
-        
+        HStack {
+            
+            Button {
+                shareSheet()
+            } label: {
+                Label("Share", systemImage: "square.and.arrow.up")
+            }.buttonStyle(PlainButtonStyle())
+            
+            Spacer()
+            
+            Button {
+                favouriteItem()
+            } label: {
+                appPrefs.isFavourite(animal: animalType, code: statusCode) ? Label("Favourite", systemImage: "star.fill") : Label("Favourite", systemImage: "star")
+            }.buttonStyle(PlainButtonStyle())
+        }
     }
     
     func favouriteItem() {
         appPrefs.favourite(animal: animalType, code: statusCode)
+    }
+    
+    //todo: ugly, needs refactoring
+    func shareSheet() {
+        let shareText = "I've just laughed at this Http Status code meme! Check it out!"
+        
+        switch animalType {
+        case .cat:
+            guard let urlShare = URL(string: "https://http.cat/\(statusCode).jpg") else { return }
+            let activityVC = UIActivityViewController(activityItems: [shareText, urlShare], applicationActivities: nil)
+            UIApplication.shared.windows.first?.rootViewController?.present(activityVC, animated: true, completion: nil)
+        case .dog:
+            guard let urlShare = URL(string: "https://http.dog/\(statusCode).jpg") else { return }
+            let activityVC = UIActivityViewController(activityItems: [shareText, urlShare], applicationActivities: nil)
+            UIApplication.shared.windows.first?.rootViewController?.present(activityVC, animated: true, completion: nil)
+        }
+        
+        
     }
 }
 
