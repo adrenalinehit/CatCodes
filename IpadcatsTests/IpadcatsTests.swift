346d61c3deae26c941304d530ad4de60abf4566a
Nick Payne <nick.payne2@skybettingandgaming.com>
2022-06-22 21:31:56 +0100

    tweak unit test

--- IpadcatsTests/IpadcatsTests.swift
+++ IpadcatsTests/IpadcatsTests.swift
@@ -18,17 +18,11 @@ final class IpadcatsTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-        var fav1 = Favourite(code: 100, animal: .dog)
-        var fav2 = Favourite(code: 100, animal: .cat)
+    func testFavouriteEquality() throws {
+        let fav1 = Favourite(code: 100, animal: .dog)
+        let fav2 = Favourite(code: 100, animal: .cat)
         
         XCTAssertNotEqual(fav1, fav2)
-        
     }
 
 }
