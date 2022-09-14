57f3b099dd864a9f5d47465525e1167393164401
Nick <adrenalinehit@users.noreply.github.com>
2022-09-14 15:18:09 +0100

    working snapshot test (#20)

---
+++ IpadcatsTests/SnapshotTester.swift
@@ -0,0 +1,47 @@
+//
+//  SnapshotTester.swift
+//  IpadcatsTests
+//
+//  Created by Nick Payne on 08/09/2022.
+//
+
+import XCTest
+import SwiftUI
+
+@testable import ipadcats
+@testable import ipadcats_data
+
+import SnapshotTesting
+
+final class SnapshotTester: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+        func testSidebarSnapshotTest() throws {
+            // UI tests must launch the application that they test.
+           //let app = XCUIApplication()
+           //app.launch()
+
+            let appView = SidebarView()
+                .environmentObject({ () -> AppPreferences in
+                    let envObj = AppPreferences()
+                    envObj.animalPreference = .cat
+                    return envObj
+                }())
+            
+            let view: UIView = UIHostingController(rootView: appView).view
+
+            assertSnapshot(
+              matching: view,
+              as: .image(size: view.intrinsicContentSize))
+        }
+    }
+
+
+
